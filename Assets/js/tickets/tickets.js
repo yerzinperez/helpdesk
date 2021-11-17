c445c85413ad271ff7388333c1ccd92a7e2529bd
Yerzin Perez <yerzin24@hotmail.com>
2021-11-16 21:06:38 -0500

    Se configuran tablas de tickets

--- Assets/js/tickets/tickets.js
+++ Assets/js/tickets/tickets.js
@@ -1,5 +1,65 @@
 $(function() {
     actualizarEstiloMenu();
+    $('#tblTicketsAbiertos').DataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        searching: true,
+        "lengthChange": true,
+        colReorder: true,
+        // "ajax": {
+        //     "url": " " + base_url + "Usuarios/getUsuarios",
+        //     "dataSrc": ""
+        // },
+        // "columns": [
+        //     { "data": "id_usuario" },
+        //     { "data": "nombres" },
+        //     { "data": "apellidos" },
+        //     { "data": "telefono" },
+        //     { "data": "correo" },
+        //     { "data": "rol" },
+        //     { "data": "estado" },
+        //     { "data": "opciones" }
+        // ],
+        "responsive": "true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order": [
+            [0, "desc"]
+        ]
+    });
+    $('#tblTicketsAtrasados').DataTable({
+        "aProcessing": true,
+        "aServerSide": true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+        },
+        searching: true,
+        "lengthChange": true,
+        colReorder: true,
+        // "ajax": {
+        //     "url": " " + base_url + "Usuarios/getUsuarios",
+        //     "dataSrc": ""
+        // },
+        // "columns": [
+        //     { "data": "id_usuario" },
+        //     { "data": "nombres" },
+        //     { "data": "apellidos" },
+        //     { "data": "telefono" },
+        //     { "data": "correo" },
+        //     { "data": "rol" },
+        //     { "data": "estado" },
+        //     { "data": "opciones" }
+        // ],
+        "responsive": "true",
+        "bDestroy": true,
+        "iDisplayLength": 10,
+        "order": [
+            [0, "desc"]
+        ]
+    });
     $('#tblTicketsCerrados').DataTable({
         "aProcessing": true,
         "aServerSide": true,
